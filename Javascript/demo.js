a04c9c50650688811b709ac820cceb59692ff8dc
ohmybaby787 <tranngocquy1820@gmail.com>
2023-05-26 19:53:56 +0700

    1

---
+++ Javascript/demo.js
@@ -0,0 +1,12 @@
+function tinhDiem3Mon() {
+    let t = prompt("input t")
+    let v = 9 
+    let a = 6 
+    let total = 2 * (t + v) + a;
+    if(total >=30){
+        alert("Pass");
+    }else{
+        alert("Fail")
+    };
+}
+tinhDiem3Mon();
