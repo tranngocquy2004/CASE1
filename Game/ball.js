328168e62cee4e72e8d806fe62eeb92306ccef8a
ohmybaby787 <tranngocquy1820@gmail.com>
2023-07-11 21:30:58 +0700

    1

--- Game/ball.js
+++ Game/ball.js
@@ -1,24 +1,19 @@
 class Ball{
     constructor(){
     this.x = 40;
+    this.dx = 8;
+    this.dy = 8;
     this.y = 40;
-    this.radius = 20
+    this.radius = 20;
     }
     display(canvas) {
-        // let canvas = document.getElementById("game");
         let ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(this.x , this.y , this.radius, 0 , Math.PI * 2);
+        ctx.fillStyle = "red"
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
-        function diChuyenbong(){
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            x += 2;
-            y += 2;
-            requestAnimationFrame(diChuyenbong);
         }
-    }
-   
 }
  
